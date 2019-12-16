0a095f716f33d72300a013a5336b11be2b450fa1
atlonis <veronika.belozechko@gmail.com>
2019-12-16 17:05:50 +0300

    add

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
 import { createStore } from "redux";
+import allReducers from "./reducers";
 
-const store = createStore();
+const store = createStore(allReducers);
 ReactDOM.render(
   <Provider store={store}>
     <App />
---
+++ src/reducers/car.js
@@ -0,0 +1,19 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      car: "Audi",
+      speed: 234.43
+    },
+    {
+      id: 2,
+      car: "BMW",
+      speed: 314.8
+    },
+    {
+      id: 3,
+      car: "Mercedes",
+      speed: 284.9
+    }
+  ];
+}
