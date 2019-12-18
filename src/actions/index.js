f217700aa91be9dc25c6b39fb4226d143df1db26
atlonis <veronika.belozechko@gmail.com>
2019-12-18 15:57:41 +0300

    add action

---
+++ src/actions/index.js
@@ -0,0 +1,6 @@
+export function sendMessage(payload) {
+  return {
+    type: "ADD_MESSAGE",
+    payload
+  };
+}
--- src/reducers/letters.js
+++ src/reducers/letters.js
@@ -1,19 +1,27 @@
-export default function() {
-  return [
-    {
-      date: "20-18-2018",
-      title: "Test title 1",
-      status: "done"
-    },
-    {
-      date: "20-11-2018",
-      title: "Test title 2",
-      status: "pending"
-    },
-    {
-      date: "20-08-2018",
-      title: "Test title 3",
-      status: "error"
-    }
-  ];
+const initialState = [
+  {
+    date: "20-18-2018",
+    title: "Test title 1",
+    status: "done"
+  },
+  {
+    date: "20-11-2018",
+    title: "Test title 2",
+    status: "pending"
+  },
+  {
+    date: "20-08-2018",
+    title: "Test title 3",
+    status: "error"
+  }
+];
+
+export default function letters(state = initialState, action) {
+  switch (action.type) {
+    case "ADD_MESSAGE":
+      return [...state, ...action.payload];
+
+    default:
+      return state;
+  }
 }
